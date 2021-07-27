7ffa9451800e936557cecf70e77aa020b18c40cc
jiaqiangmo <mjqamjq@gmail.com>
2021-07-27 17:41:22 +0800

    feat: Thunk Functions

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
+import CounterReducer from '../features/counter/counterSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
-    users: usersReducer
+    users: usersReducer,
+    counter: CounterReducer
   }
 })
